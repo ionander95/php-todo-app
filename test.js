11c25ffc0114fe551bff28fb00d6ca45e92d75dc
ionander95 <villegas_95@hotmail.com>
2016-09-21 07:33:48 +0000

    commit3

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $.getJSON( "getLists.php", function( data ) {
         console.log(data);
+        $(contentDiv).html(data[0].'ID');
+        $(contentDiv2).html(data[0].'Nombre');
     });
 });
